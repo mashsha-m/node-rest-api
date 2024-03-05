795ec7edb7c39eb8ef71a1eeffb4cdfea2e479f1
Masha Zyryanova <miskusitel@gmail.com>
2024-03-05 16:50:52 +0500

    rewritten

--- routes/servers.js
+++ routes/servers.js
@@ -1,8 +1,11 @@
-import {Router} from "express"
+/*import {Router} from "express"
 import {getAll, create, deleteEl} from "../controllers/servers.js"
+const router = Router()*/
+const Router = require('express')
 const router = Router()
+const controllers = require('../controllers/servers.js')
 
-router.get('/api/server', getAll)
-router.post('/api/server', create)
-router.delete('/api/server/:id', deleteEl)
-export default router
+router.get('/api/server', controllers.getAll)
+router.post('/api/server', controllers.create)
+router.delete('/api/server/:id', controllers.deleteEl)
+module.exports = router
