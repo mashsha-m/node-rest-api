29da6b8742d5dc0d8118bb4af083152aaaf30bd9
Masha Zyryanova <miskusitel@gmail.com>
2024-03-05 16:49:45 +0500

    rewritten

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-import express from 'express'
-import path from 'path'
-import {requestTime, sayHi} from './middlewares.js'
-import serverRoutes from './routes/servers.js'
+const http = require('http');
+const express = require('express');
+const fetch = require('node-fetch')
+const serverRoutes = require('./routes/servers.js')
 
-const __dirname = path.resolve()
-const PORT = process.env.PORT ?? 3000
 const app = express()
-
-app.set('view engine', 'ejs')
-app.set('views', path.resolve(__dirname, 'ejs'))
-//console.log(app.get('views'))
-
-app.use(express.static(path.resolve(__dirname, 'client')))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(requestTime)
-//app.use(sayHi)
-
 app.use(serverRoutes)
-app.get('/', (req, res) => {
-    res.render('index', {title: 'this is title'})
-})
-app.get('/next', (req, res) => {
-    res.render('next')
-})
-app.get('/form', (req, res) => {
-    res.render('form')
-})
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('Hello World! -Маша');
+});
+
+let router = express.Router()
+
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+router.post('/node/webhookMethod', async function(req, res) {
+
+    let webhook = req.body.webhook;
+    let method = req.body.method;
+    let params = req.body.params;
+    try {
+        const response = await fetch(webhook+method, {
+            method: 'POST',
+            body: JSON.stringify(params),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        const data = await response.json();
+        res.status(201).json(data);
+    }
+    catch (e) {
+        res.status(400).json(e.message)
+    }
+
+});
+
+router.route('/autoPM/')
+    .get(function (req, res, next) {
+        //res.json(req.params)
+        res.send('Hi, application~')
+        //res.sendFile(path.resolve(__dirname, 'client', 'next.html'))
+    })
+    .post(function(req, res) {
+        res.send('Add a book');
+        res.json()
+    })
 /*app.get('/next', (req, res) => {
     //console.log(req.requestTime)
     res.sendFile(path.resolve(__dirname, 'client', 'next.html'))
     //res.download(path.resolve(__dirname, 'client', 'next.html')) - скачать указанную страницу
 })*/
-app.listen(PORT, () => {
-    console.log("сервер сервер "+PORT)
-})
+app.use(router)
+
+app.listen(3000);
